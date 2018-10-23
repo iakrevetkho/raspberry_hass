b3f12d5e41cb3d2812395de78a3d236124ee9b8a
iakrevetkho <40871973+iakrevetkho@users.noreply.github.com>
2018-10-23 15:11:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,13 +17,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update -y
 
 # Install docker-ce:
-sudo apt-get install docker-ce -y
-
-# Install docker-compose
-# Download
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-# Apply permissions
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get install docker.io -y
 
 # Delete all previously containers
 sudo docker rm $(sudo docker ps -a -q) -f
